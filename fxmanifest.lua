dcfb5826aa2a93556c7ec4a4f4c42fcddec8d775
Max <maxsponer9@gmail.com>
2025-06-20 03:34:57 +0200

    forgor that

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
+
+ui_page 'nui/index.html'
+
+files {
+    'nui/index.html',
+    'data/audioexample_sounds.dat54.rel',
+    'audiodirectory/custom_sounds.awc',
+}
+
+data_file 'AUDIO_WAVEPACK' 'audiodirectory'
+data_file 'AUDIO_SOUNDDATA' 'data/audioexample_sounds.dat'
